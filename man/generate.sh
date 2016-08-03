02bec74d2b2a16a614674ceab7da082827a5b680
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2016-08-04 01:36:56 +0200

    add inital building and man page for the gettz utility

--- man/generate.sh
+++ man/generate.sh
@@ -10,6 +10,9 @@
 
 a2x -fpdf timezoned.asciidoc -a revdate="`date`" -v -v
 a2x -f manpage timezoned.asciidoc -v -v
-a2x -f docbook timezoned.asciidoc -v -v
-a2x -f xhtml timezoned.asciidoc -v -v
+#a2x -f docbook timezoned.asciidoc -v -v
+#a2x -f xhtml timezoned.asciidoc -v -v
+
+a2x -fpdf gettz.asciidoc -a revdate="`date`" -v -v
+a2x -f manpage gettz.asciidoc -v -v
 
